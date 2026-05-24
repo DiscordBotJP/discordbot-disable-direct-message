7c54f5a4ddb14d76552f274b72f434bd4b2fb929
Shun.T <1ntegrale9uation@gmail.com>
2026-05-25 07:43:25 +0900

    feat: add ops-log-hub settings

--- constants/__init__.py
+++ constants/__init__.py
@@ -4,3 +4,12 @@ from os import getenv
 load_dotenv()
 
 TOKEN = getenv('DISCORD_BOT_TOKEN')
+
+OPS_LOG_HUB_URL = getenv(
+    'OPS_LOG_HUB_URL',
+    'https://ops-log-hub.up.railway.app/api/ingest/discord-bot',
+)
+OPS_LOG_HUB_KEY = getenv('OPS_LOG_HUB_KEY')
+OPS_LOG_HUB_ENVIRONMENT = getenv('OPS_LOG_HUB_ENVIRONMENT', 'production')
+OPS_LOG_HUB_PROJECT = getenv('OPS_LOG_HUB_PROJECT', 'discordbot_disable_direct_message')
+OPS_LOG_HUB_TIMEOUT_SECONDS = float(getenv('OPS_LOG_HUB_TIMEOUT_SECONDS', '5'))
